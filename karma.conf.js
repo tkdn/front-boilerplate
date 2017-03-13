e0bba70ce3ca0cc1b4b6f685c1b54ab0aa4921fc
Satoshi Takeda <tkdnation@gmail.com>
2017-03-14 06:44:31 +0900

    ✍️  log reporter

--- karma.conf.js
+++ karma.conf.js
@@ -6,6 +6,21 @@ delete webpackConfig.output
 module.exports = function(config) {
   config.set({
     frameworks: ['mocha', 'sinon'],
+    reporters: ['mocha'],
+    mochaReporter: {
+      colors: {
+        success: 'blue',
+        info: 'bgGreen',
+        warning: 'cyan',
+        error: 'bgRed'
+      },
+      symbols: {
+        success: '+',
+        info: '#',
+        warning: '!',
+        error: 'x'
+      }
+    },
     files: [
       {
         pattern: 'test/**/*.spec.js', watched: false
