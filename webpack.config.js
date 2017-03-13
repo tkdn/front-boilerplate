b952031b87ca933f3e99281e07894237e65068f3
Satoshi Takeda <tkdnation@gmail.com>
2017-03-14 07:31:21 +0900

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,6 @@ const webpackConfig = {
         new webpack.DefinePlugin({
           'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
         }),
-        new webpack.optimize.DedupePlugin(),
         new webpack.optimize.UglifyJsPlugin({minimize: true})
       ]
     }
